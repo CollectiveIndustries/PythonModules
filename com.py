2489977bb165b06c8ef00d58900350f7b5cb9bd2
Andrew Malone <admmorketh@gmail.com>
2018-09-21 00:40:36 -0500

    Moves _OS_ class into a common module since we use this everywhere

--- com.py
+++ com.py
@@ -3,6 +3,53 @@
 ## Hopefully we can avoid disaster if we dont import this in a main program
 import os, shutil
 
+## OS Specific Stuff
+class _OS_(object):
+    def __init__(self):
+        _SystemOS_ = platform.strip()
+        if (_SystemOS_ == 'linux' or _SystemOS_ == 'linux2'):
+        # linux
+            with open('/etc/os-release') as file:
+                oper = file.readlines()
+                oper = oper[5].split('=')
+                self._type_ = oper[1].strip() # Grab OS release Name we want to know what flavor of lenny we use.
+        elif(_SystemOS_ == 'win32'):
+            self._type_ = _SystemOS_
+        
+    def Clear(self):
+        if(self._type_ == "win32"):
+            os.system("cls")
+        else: # well its not Windows we can just "clear"
+            os.system("clear")
+
+    def Input(self, prompt):
+        if(self._type_ == "win32"):
+            return input(prompt)
+        elif(self._type_ == "debian"):
+            return raw_input(prompt)
+
+    def Shutdown(self):
+        if(_OS_._type_ == 'win32'):
+            os.system('shutdown', '/s')
+        elif(_OS_._type_ == 'debian'):
+            os.system('sudo shutdown -h')
+
+    def Reboot(self):
+        if(_OS_._type_ == 'win32'):
+            os.system('shutdown', '/r')
+        elif(_OS_._type_ == 'debian'):
+            os.system('sudo reboot')
+
+     # TODO Make sure that these directories exist before moving files around
+#    def mv(dir_src,dir_dst):
+#        for file in os.listdir(dir_src):
+#            print("{}Installing: {}{}".format(color.HEADER,file,color.END))
+#            src_file = os.path.join(dir_src, file)
+#            dst_file = os.path.join(dir_dst, file)
+#            shutil.copyfile(src_file, dst_file)
+
+            ## END OF _OS_ CLASS
+
 # Text output color definitions
 class color:
     HEADER = '\033[95m'
@@ -16,11 +63,3 @@ class color:
 
 def shellQoute(str):
 	return "'" + str.replace("'", "'\\''") + "'"
-
-# TODO Make sure that these directories exist before moving files around
-def mv(dir_src,dir_dst):
-	for file in os.listdir(dir_src):
-		print("{}Installing: {}{}".format(color.HEADER,file,color.END))
-		src_file = os.path.join(dir_src, file)
-		dst_file = os.path.join(dir_dst, file)
-		shutil.copyfile(src_file, dst_file)
